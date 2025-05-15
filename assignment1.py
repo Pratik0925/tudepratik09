aebf73a076253df2d2837bdb557e5122258b2807
Pratik0925 <pghatge46@gmail.com>
2025-05-15 10:42:02 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+
+c=input("Enter the first number: ")
+d=input("Enter the second number: ")
+
+sum=int(c) + int(d)
+sub=int(c) - int(d)
+mul=int(c) * int(d)
+divi=int(c) / int(d)
+
+print("Additon: ", sum)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",divi)
+
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+print("Hello, "+ a +" "+ b +"! Welcome to the Python program." )
